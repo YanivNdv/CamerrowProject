fecd91b072e80c9fd08baf8fe6194757957a2d28
yanivnadav <developer.yanivnadav93@gmail.com>
2018-09-03 22:29:05 +0300

    Menu Bar

--- app/src/main/java/com/camerrow/camerrowproject/MainActivity.java
+++ app/src/main/java/com/camerrow/camerrowproject/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -78,7 +80,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void checkUserExist() {
 
